eb5fbfb77c7a2822e1e072e1173a8c6282d962e9
edwinromero <ecro98.98@gmail.com>
2022-05-11 10:44:06 -0500

    -Modificar elementos de un arreglo -Agregar elementos a un arreglo

--- arreglos/arreglos.js
+++ arreglos/arreglos.js
@@ -8,7 +8,7 @@ let autos = new Array('BMW', 'Mercedez Benz', 'Volvo');
 const autos = ['BMW', 'Mercedez Benz', 'Volvo']
 console.log(autos)
 
-//Obtener un valor de un arreglo
+//Obtener un valor de un arreglo único
 console.log(autos[2]);
 
 
@@ -16,3 +16,13 @@ console.log(autos[2]);
 for (i; i<autos.length; i++){
     console.log(i+ ': '+ autos[i]);
 }
+
+//Modificar elementos de un arreglo
+
+autos[1]='Chevrolet';
+console.log(autos);
+
+//Agregar elementos a un arreglo
+
+autos.push('Audi');
+console.log(autos);
