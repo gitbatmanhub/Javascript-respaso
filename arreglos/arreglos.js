d41524ad2cb5e2b45d62821e86e40069bc5c4788
edwinromero <ecro98.98@gmail.com>
2022-05-11 10:39:52 -0500

    Recorrer los Elementos de un Arreglo

--- arreglos/arreglos.js
+++ arreglos/arreglos.js
@@ -1,4 +1,4 @@
-
+let i = 0;
 // Manera tradicional
  /*
 let autos = new Array('BMW', 'Mercedez Benz', 'Volvo');
@@ -7,3 +7,12 @@ let autos = new Array('BMW', 'Mercedez Benz', 'Volvo');
 
 const autos = ['BMW', 'Mercedez Benz', 'Volvo']
 console.log(autos)
+
+//Obtener un valor de un arreglo
+console.log(autos[2]);
+
+
+//recorrer un arreglo
+for (i; i<autos.length; i++){
+    console.log(i+ ': '+ autos[i]);
+}
