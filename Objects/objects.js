c40e91c5376593f43d0ecae76bdcdcd499c62050
edwinromero <ecro98.98@gmail.com>
2022-06-01 07:59:00 -0500

    Ejemplo: Agregar Métodos a Objetos en JS

--- Objects/objects.js
+++ Objects/objects.js
@@ -6,10 +6,14 @@ let persona ={
         nombre : "Edwin",
         apellido: "Romero",
         email: "correo@gmail.com",
-        edad: 23
+        edad: 23,
+        nombreCompleto: function (){
+                return this.nombre + ' ' + this.apellido;
+        }
 }
 
 
 console.log(persona.nombre);
 console.log(persona.apellido);
 console.log(persona.edad);
+console.log(persona.nombreCompleto())
