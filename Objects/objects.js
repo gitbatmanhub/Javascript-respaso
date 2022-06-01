5a166ab25f0fdc10695d6229124093b66520ed52
edwinromero <ecro98.98@gmail.com>
2022-06-01 07:54:19 -0500

    Ejemplo: Objetos en Js

---
+++ Objects/objects.js
@@ -0,0 +1,15 @@
+
+
+
+
+let persona ={
+        nombre : "Edwin",
+        apellido: "Romero",
+        email: "correo@gmail.com",
+        edad: 23
+}
+
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.edad);
