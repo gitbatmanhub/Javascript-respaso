828690aa4e3345849551ef72b9a7dc685f1e13df
edwinromero <ecro98.98@gmail.com>
2022-06-01 10:53:09 -0500

    Ejemplo: Método Apply en Js

--- Objects/objects.js
+++ Objects/objects.js
@@ -2,7 +2,8 @@ let persona1={
         nombre: 'Edwin',
         apellido: 'Romero',
         nombreCompleto: function (titulo, tel){
-                return titulo+ ': '+ this.nombre + ' '+ this.apellido +' ' + tel;
+                //return this.nombre+ ' '+ this.apellido;
+               return titulo+ ': '+ this.nombre + ' '+ this.apellido +' ' + tel;
         }
 }
 
@@ -11,9 +12,12 @@ let persona2 = {
         apellido: 'Torres'
 }
 
-//Uso de call para usar el metodo nombre completo usando el metodo de p1 en p2
+//Uso de apply para usar el metodo nombre completo usando el metodo de p1 en p2
 console.log(persona1.nombreCompleto('Lic', '0999017549'));
 
-console.log(persona1.nombreCompleto.call(persona2, 'Ing', '0967792636'));
+
+//Puedo pasar la variable que contiene el array o el content del array
+//let arreglo = ['Ing', '0961179236'];
+console.log(persona1.nombreCompleto.apply(persona2, ['Ing', '0961179236']));
 
 
