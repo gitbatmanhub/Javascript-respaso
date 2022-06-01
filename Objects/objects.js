bdcfaddbcd42705ac46cf2284b38b3ffb4428c85
edwinromero <ecro98.98@gmail.com>
2022-06-01 09:28:42 -0500

    Ejemplo: Método get en Objetos de Js

--- Objects/objects.js
+++ Objects/objects.js
@@ -7,23 +7,8 @@ let persona ={
         apellido: "Romero",
         email: "correo@gmail.com",
         edad: 23,
-        nombreCompleto: function (){
+        get nombreCompleto (){
                 return this.nombre + ' ' + this.apellido;
         }
 }
-
-
-persona.tel = '0967792636';
-delete persona.tel;
-
-console.log(persona);
-
-for (nombrePropiedad in persona){
-        console.log(persona[nombrePropiedad]);
-}
-
-let personaArray = Object.values(persona);
-console.log(personaArray)
-
-let personaString = JSON.stringify (persona);
-console.log(personaString)
+console.log(persona.nombreCompleto)
