1bca584057873260448e997938e5152f48750f8e
edwinromero <ecro98.98@gmail.com>
2022-06-01 15:36:08 -0500

    Ejemplo: Herencia en JS

--- Clases/clases.js
+++ Clases/clases.js
@@ -10,8 +10,34 @@ class Persona{
     set nombre(nombre){
         this._nombre= nombre;
     }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido= apellido;
+    }
+
+}
+class Empleado extends Persona{
+    constructor(nombre, apellido, departamento) {
+        super(nombre, apellido);//Llamar al constructor de la clase padre
+        this._departamento= departamento;
+    }
+    get departamento(){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento=departamento;
+    }
 }
 
 let persona1 = new Persona('Edwin', 'Romero');
 persona1.nombre='Cesar';  //Set nombre
 console.log(persona1.nombre); //Get Nombre
+
+let empleado1 = new Empleado('Nathaly','Torres', 'Docencia');
+console.log(empleado1);
+empleado1.departamento='Sistema';
+
