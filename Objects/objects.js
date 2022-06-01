a3a2f91d406fb7f0c2f31532fc8e29371b1793f2
edwinromero <ecro98.98@gmail.com>
2022-06-01 10:20:27 -0500

    Ejemplo: Agregar Métodos a un Método Constructor de Objetos

--- Objects/objects.js
+++ Objects/objects.js
@@ -6,10 +6,13 @@ function Persona(nombre, apellido, email){
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
+        this.nombreCompleto= function (){
+                return this.nombre + ' ' + this.apellido;
+        }
 }
 
 
 let padre = new Persona('Edwin', 'Romero', 'correo@gmail.com');
-console.log(padre);
+console.log(padre.nombreCompleto());
 let madre = new Persona('Maria', 'Romero', 'correo@gmail.com');
-console.log(madre);
+console.log(madre.nombreCompleto());
