08fc89576b9a0449c03102c7d98bdfe1e7ab0134
edwinromero <ecro98.98@gmail.com>
2022-06-01 15:00:18 -0500

    Ejemplo: Clases en JavaScript

---
+++ Clases/clases.js
@@ -0,0 +1,11 @@
+class Persona{
+    constructor(nombre, apellido) {
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
+}
+
+let persona1 = new Persona('Edwin', 'Romero');
+console.log(persona1);
+let persona2 = new Persona('Nathaly', 'Torres');
+console.log(persona2);
